2d4887614a5b34da65cf95800552dc2372b74a83
Maicol <maicolmontoya323@gmail.com>
2023-10-24 00:23:39 -0500

    Project started

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.infinitehorizons.taskmanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+    requires java.sql;
+    requires mysql.connector.j;
+    requires java.desktop;
+
+    opens com.infinitehorizons.taskmanager to javafx.fxml;
+    exports com.infinitehorizons.taskmanager;
+}
