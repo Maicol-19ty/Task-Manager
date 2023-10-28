fb64afe30c3bf99fc4098f99674c202f1e14a74b
Maicol <maicolmontoya323@gmail.com>
2023-10-28 15:55:25 -0500

    Changed Main FMXL Route

--- src/main/java/com/infinitehorizons/taskmanager/Main.java
+++ src/main/java/com/infinitehorizons/taskmanager/Main.java
@@ -1,7 +1,8 @@
 package com.infinitehorizons.taskmanager;
 
-import com.infinitehorizons.taskmanager.DataBase.Connect;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -10,12 +11,16 @@ import java.io.IOException;
 
 public class Main extends Application {
 
+    protected ObservableList<Image> icons = FXCollections.observableArrayList();
+
     @Override
     public void start(Stage stage) throws IOException {
 
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.getIcons().add(new Image("file:src/main/java/com/infinitehorizons/taskmanager/Images/task-logo.jpeg"));
+        stage.getIcons().add(new Image("file:src/main/resources/com/infinitehorizons/taskmanager/images/task-logo.png"));
+        stage.getIcons().addAll(icons);
         stage.setTitle("Task Manager - Login");
         stage.setScene(scene);
         stage.show();
