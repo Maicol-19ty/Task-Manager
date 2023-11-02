3e4e1b95451a30dd9fcc1150fbad5e45fb29c066
Maicol <maicolmontoya323@gmail.com>
2023-11-01 20:31:28 -0500

    Initialized image created

--- src/main/java/com/infinitehorizons/taskmanager/Controllers/ForgotController.java
+++ src/main/java/com/infinitehorizons/taskmanager/Controllers/ForgotController.java
@@ -1,4 +1,31 @@
 package com.infinitehorizons.taskmanager.Controllers;
 
-public class ForgotController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ForgotController implements Initializable {
+
+    @FXML
+    private ImageView forbrandingImageView;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Pestaña de recuerdo inicializada.");
+        File forbrandingFile = new File("src/main/resources/com/infinitehorizons/taskmanager/images/task-logo-2.png");
+        if (forbrandingFile.exists()) {
+            Image forbrandingImage = new Image(forbrandingFile.toURI().toString());
+            forbrandingImageView.setImage(forbrandingImage);
+        } else {
+            System.out.println("No se pudo encontrar la imagen en la ubicación especificada.");
+        }
+
+    }
+
 }
