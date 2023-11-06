b6a0ee123d1a794f5478232bb47a76f043ea0c25
Maicol <maicolmontoya323@gmail.com>
2023-11-06 16:41:22 -0500

    Registration and redirection logic completed

--- src/main/java/com/infinitehorizons/taskmanager/Controllers/RegisterController.java
+++ src/main/java/com/infinitehorizons/taskmanager/Controllers/RegisterController.java
@@ -1,6 +1,8 @@
 package com.infinitehorizons.taskmanager.Controllers;
 
-
+import com.infinitehorizons.taskmanager.DataBase.Connect;
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.value.ObservableValue;
@@ -19,19 +21,22 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-
-import javax.swing.*;
 import javafx.beans.value.ChangeListener;
+import javafx.util.Duration;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Date;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class RegisterController implements Initializable {
 
-    private Stage stage;
-    private Scene scene;
     @FXML
     private ImageView regbrandingImageView;
     @FXML
@@ -43,8 +48,6 @@ public class RegisterController implements Initializable {
     @FXML
     private Button registerButton;
     @FXML
-    private Button loginButton;
-    @FXML
     private TextField emailTextField;
     @FXML
     private TextField firstnameTextField;
@@ -54,8 +57,6 @@ public class RegisterController implements Initializable {
     private TextField usernameTextField;
     @FXML
     private Label passwordMatchLabel;
-
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -70,7 +71,7 @@ public class RegisterController implements Initializable {
         }
 
         BooleanBinding passwordMatch = Bindings.createBooleanBinding(() ->
-                passwordTextField.getText().equals(confpasswordTextField.getText()),
+                        passwordTextField.getText().equals(confpasswordTextField.getText()),
                 passwordTextField.textProperty(), confpasswordTextField.textProperty());
 
         passwordMatch.addListener(new ChangeListener<Boolean>() {
@@ -88,16 +89,20 @@ public class RegisterController implements Initializable {
 
     @FXML
     private void passwordKeyRelease(KeyEvent event) {
-        if (passwordTextField.getText().equals(confpasswordTextField.getText())) {
+        if (passwordTextField.getText().equals(confpasswordTextField.getText()) && passwordTextField.getText().length() >= 8 && confpasswordTextField.getText().length() >= 8) {
             passwordMatchLabel.setText("✅ You are set");
+        } else if (passwordTextField.getText().equals(confpasswordTextField.getText()) && passwordTextField.getText().length() < 8 && confpasswordTextField.getText().length() < 8){
+            passwordMatchLabel.setText("❌ Password too short");
         } else {
             passwordMatchLabel.setText("❌ Do not match");
         }
     }
 
     public void confirmPass() {
-        if (passwordTextField.getText().equals(confpasswordTextField.getText())) {
+        if (passwordTextField.getText().equals(confpasswordTextField.getText()) && passwordTextField.getText().length() >= 8 && confpasswordTextField.getText().length() >= 8) {
             passwordMatchLabel.setText("✅ You are set");
+        } else if (passwordTextField.getText().equals(confpasswordTextField.getText()) && passwordTextField.getText().length() < 8 && confpasswordTextField.getText().length() < 8){
+            passwordMatchLabel.setText("❌ Password too short");
         } else {
             passwordMatchLabel.setText("❌ Do not match");
         }
@@ -105,8 +110,8 @@ public class RegisterController implements Initializable {
 
     public void loginButtonOnAction (ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/infinitehorizons/taskmanager/login.fxml")));
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root, 1520, 790);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 1520, 790);
         stage.setTitle("Task Manager - Login");
         stage.setScene(scene);
         stage.show();
@@ -114,19 +119,108 @@ public class RegisterController implements Initializable {
 
     public void registerButtonOnAction (ActionEvent event) throws IOException {
         registerMessageLabel.setText("Failed to register, please try again");
-        confirmPass();
 
         if (!emailTextField.getText().isBlank() && !firstnameTextField.getText().isBlank() && !lastnameTextField.getText().isBlank() && !usernameTextField.getText().isBlank() && !passwordTextField.getText().isBlank() && !confpasswordTextField.getText().isBlank()) {
-
+            confirmPass();
+            startRegisteringAnimation();
+            register();
         } else {
             registerMessageLabel.setText("Please enter all fields");
         }
-
     }
 
+    public void register () {
 
+        String checkUsername = "SELECT * FROM user_accounts WHERE username = '" + usernameTextField.getText() + "'";
 
+        Connect connectNow = new Connect();
+        Connection connectDB = connectNow.getConnection();
 
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet resultSet = statement.executeQuery(checkUsername);
 
+            if (resultSet.next()) {
+                registerMessageLabel.setText(usernameTextField.getText() + " is already in use");
+                stopRegisteringAnimation();
+            } else {
+
+                registerMessageLabel.setText("Registering...");
+
+                String insertData = "INSERT INTO user_accounts (email, firstname, lastname, username, password, date) VALUES (?, ?, ?, ?, ?, ?)";
+
+                PreparedStatement preparedStatement = connectDB.prepareStatement(insertData);
+
+                preparedStatement.setString(1, emailTextField.getText());
+                preparedStatement.setString(2, firstnameTextField.getText());
+                preparedStatement.setString(3, lastnameTextField.getText());
+                preparedStatement.setString(4, usernameTextField.getText());
+                preparedStatement.setString(5, passwordTextField.getText());
+
+                Date date = new Date();
+                java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+
+                preparedStatement.setString(6, String.valueOf(sqlDate));
+
+                preparedStatement.executeUpdate();
+
+                registerMessageLabel.setText("User registered successfully");
+
+                registerClearFields();
+
+                redirectToMainPage();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    public void registerClearFields () {
+        emailTextField.setText("");
+        firstnameTextField.setText("");
+        lastnameTextField.setText("");
+        usernameTextField.setText("");
+        passwordTextField.setText("");
+        confpasswordTextField.setText("");
+        confpasswordTextField.setText("");
+    }
+
+    public void redirectToMainPage() {
+        PauseTransition pause = new PauseTransition(Duration.seconds(3));
+        pause.setOnFinished(event -> {
+            try {
+                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/infinitehorizons/taskmanager/home.fxml")));
+                Stage stage = (Stage) registerButton.getScene().getWindow();
+                Scene scene = new Scene(root, 1520, 790);
+                stage.setTitle("Task Manager - Home");
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        pause.play();
+    }
+
+    public void startRegisteringAnimation() {
+        registerMessageLabel.setText("Registering...");
+        registerMessageLabel.setVisible(true);
+
+        FadeTransition fade = new FadeTransition(Duration.seconds(3), registerMessageLabel);
+        fade.setFromValue(0.0);
+        fade.setToValue(1.0);
+        fade.play();
+    }
+
+    public void stopRegisteringAnimation() {
+        FadeTransition fade = new FadeTransition(Duration.seconds(3), registerMessageLabel);
+        fade.setFromValue(1.0);
+        fade.setToValue(0.0);
+        fade.setOnFinished(event -> {
+            registerMessageLabel.setVisible(false);
+        });
+        fade.play();
+    }
 
 }
