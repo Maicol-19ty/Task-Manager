8b2a06c2c9f559458769bef9b7b7ac0e8d176e9d
Maicol <maicolmontoya323@gmail.com>
2023-11-11 12:59:38 -0500

    Style added to fields

--- src/main/java/com/infinitehorizons/taskmanager/Main.java
+++ src/main/java/com/infinitehorizons/taskmanager/Main.java
@@ -8,8 +8,8 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 
@@ -17,16 +17,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),1520,790);
         stage.getIcons().add(new Image("file:src/main/resources/com/infinitehorizons/taskmanager/images/task-logo-2.png"));
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/infinitehorizons/taskmanager/Style/Style.css")).toExternalForm());
         stage.getIcons().addAll(icons);
         stage.setTitle("Task Manager - Login");
         stage.setScene(scene);
         stage.show();
     }
-
 
     public static void main(String[] args) {
         launch();
