e39059a8a7c571beabc27e73d31b2b132f9a1b00
Maicol <maicolmontoya323@gmail.com>
2023-11-22 01:42:15 -0500

    Initializer created and username recognition added

--- src/main/java/com/infinitehorizons/taskmanager/Controllers/HomeController.java
+++ src/main/java/com/infinitehorizons/taskmanager/Controllers/HomeController.java
@@ -1,4 +1,33 @@
 package com.infinitehorizons.taskmanager.Controllers;
 
-public class HomeController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+
+    private String userName;
+    @FXML
+    private ImageView homeImage;
+    @FXML
+    private Label usernameHome;
+
+    public HomeController(String userName) {
+        this.userName = userName;
+    }
+
+    public void initialize (URL url, ResourceBundle resourceBundle) {
+        File brandingFile = new File("src//main//resources//com//infinitehorizons//taskmanager//images//task-logo-2.png");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        homeImage.setImage(brandingImage);
+
+        usernameHome.setText(userName);
+    }
+
 }
